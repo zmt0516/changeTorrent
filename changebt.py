5217d6308133e6dd8e81ac8af6b1f7bfc8d068e7
zmt0516 <zp2.zmt@gmail.com>
2014-05-20 16:16:21 -0400

    Make more torrent can use.

--- changebt.py
+++ changebt.py
@@ -13,6 +13,11 @@ def changebt(bts):
     try:
         for i in ['publisher', 'piece length', 'publisher.utf-8', 'publisher-url.utf-8', 'publisher-url']:
             bd['info'][i]=''
+        try:
+            for i in ['comment','comment.utf-8']:
+                bd['info'][i]=''
+        except:
+            pass
         for i in ['name','name.utf-8']:
             ## If U can not use the torrent also, add the code next line
             #bd['info'][i]=transpy.getpinyin(bd['info'][i])
@@ -20,11 +25,20 @@ def changebt(bts):
         filelist=bd['info']['files']
         fl=[[i['length'],i] for i in filelist]
         fl=[max(fl)[1]]
-        fl2=[i for i in filelist if i['length']>100*1024]
+        ## U can set the next line
+        fl2=[i for i in filelist if i['length']>10000*1024]
+        
         if len(fl2)!=0:
             bd['info']['files']=fl2
         else:
             bd['info']['files']=fl
+        
+        for i in range(len(bd['info']['files'])):
+            ## If U can not use the torrent also, add the code next lines
+            #bd['info']['files'][i]['path']=[transpy.getpinyin(j) for j in bd['info']['files'][i]['path'] ]
+            #bd['info']['files'][i]['path.utf-8']=[transpy.getpinyin(j) for j in bd['info']['files'][i]['path.utf-8'] ]
+            pass
+        
     except:
         print 'Can not tanslate the torrent string.'
         return bts
